59b1f57e6f72802f8f0aeefdf8750f16e8f43818
yohpan <yohanpaya74@hotmail.fr>
2025-05-31 19:12:09 +0200

    Add more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::{
     thread,
 };
 
+/// The ThreadPool that will contain all workers.
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: Option<mpsc::Sender<Job>>,
@@ -10,13 +11,20 @@ pub struct ThreadPool {
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+/// The Worker is responsible to handle the thread.
+/// It have an ID to be easily retrivable.
 pub struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
 }
-pub struct PoolCreationError;
 
 impl Worker {
+    /// Create a new Worker.
+    ///
+    /// The id is the id of the Worker and receiver is a thread spawed to handle infinite loop the receive from the sender.
+    /// The new thread spawned will lock the receiver to disable the ability of others Worker to use it in the same time
+    /// and it will wait to receiv something.
+    /// Then if a message is received, it will match if the receiver is always running
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
@@ -47,10 +55,6 @@ impl ThreadPool {
     /// Create a new ThreadPool.
     ///
     /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
@@ -70,6 +74,9 @@ impl ThreadPool {
         }
     }
 
+    /// Execute a callable.
+    ///
+    /// The f is the callable. The f will be wrap in a Box and send.
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
@@ -81,6 +88,9 @@ impl ThreadPool {
 }
 
 impl Drop for ThreadPool {
+    /// Override the drop method from Drop Trait.
+    ///
+    /// The self must be mutable because we have to modify workers that is own by self.
     fn drop(&mut self) {
         drop(self.sender.take());
 
